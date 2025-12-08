623b384c00a88b9e4b21cee32d7bc7ece700af6c
Abtin Okhovat <abtinokhovat@gmail.com>
2025-12-08 11:28:29 +0330

    review: mongoiterator added todo list

--- pkg/iterator/mongoIterator/mongoIterator.go
+++ pkg/iterator/mongoIterator/mongoIterator.go
@@ -1,3 +1,4 @@
+// TODO: package names should not be in camel case, use mongo_iterator instead of mongoIterator or mongoiterator, also the folder name should match the package name
 package mongoIterator
 
 import (
@@ -11,6 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// TODO: better naming for this interface, maybe ZeroChecker?
 type X interface {
 	IsZero() bool
 }
@@ -24,6 +26,7 @@ type MongoIterator struct {
 }
 
 func NewMongoIterator(col *mongo.Collection, batchSize int64) *MongoIterator {
+	// TODO: always set defaults for your configuration values, what if the batchSize is 0 or negative?
 	m := &MongoIterator{
 		col:       col,
 		batchSize: batchSize,
@@ -41,6 +44,7 @@ func (m *MongoIterator) Next(ctx context.Context) error {
 		SetSort(bson.M{"_id": 1}).
 		SetLimit(m.batchSize)
 
+	// TODO: close the cursor after use to avoid resource leak
 	cursor, err := m.col.Find(ctx, filter, opts)
 	if err != nil {
 		return err
@@ -53,18 +57,23 @@ func (m *MongoIterator) Next(ctx context.Context) error {
 	}
 	if len(m.batch) > 0 {
 		doc := m.batch[len(m.batch)-1]
+		// TODO: unsafe type assertion, this can lead to panic, always check the type assertion
 		m.lastID = doc["_id"].(primitive.ObjectID)
 	}
 	atomic.AddInt64(&m.nextCounter, 1)
+	// TODO: use a proper logger instead of log.Printf, use slog.Logger
 	log.Printf("next counter: %d", atomic.LoadInt64(&m.nextCounter))
 	return nil
 }
 
+// TODO: GetBatch, or CurrentBatch are better names, Batch is confusing, because it sounds it is creating a new batch
 func (m *MongoIterator) Batch() []bson.M {
 	return m.batch
 }
 
 func (m *MongoIterator) HasNext() bool {
+	// TODO: instead of checking for empty ID here, you can compare the length of original batch with the current batch size
+	// for example, if the original batch size is 50, and the current batch size is 43, then there is no more data to read, and this is the last batch
 	if len(m.batch) == 0 && !m.lastID.IsZero() {
 		return false
 	}
